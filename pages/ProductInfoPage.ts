679e4d8ac5f4f28563115e8d2127beee6ced5233
MallammaHR <mallumanish17@gmail.com>
2025-12-06 03:23:34 -0600

    Added utils file

--- pages/ProductInfoPage.ts
+++ pages/ProductInfoPage.ts
@@ -1,5 +1,5 @@
 import { Locator, Page } from '@playwright/test';
-import { ElementUtil } from '../utils/ElementUtil';
+import { ElementUtil } from  '../utils/ElementUtil';
 
 export class ProductInfoPage {
 
